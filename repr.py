428a2df3b904fdc40a267c3e07f9709e1978e429
Lunarmagpie <bambolambo0@gmail.com>
2023-04-15 16:45:49 -0400

    repr example

---
+++ repr.py
@@ -0,0 +1,17 @@
+class MyCoolClass:
+    def __init__(self, *, value: int):
+        self.value = value
+
+    def __repr__(self):
+        """Repr prints the python code to create this python object."""
+        return f"MyCoolClass(value={repr(self.value)})"
+
+
+if __name__ == "__main__":
+    my_class = MyCoolClass(value=10)
+
+    class_repr = repr(my_class)
+
+    print("The repr is:", class_repr)
+    # Evaling the repr should give us an identical object.
+    print("The evaled repr is:", repr(eval(class_repr)))
