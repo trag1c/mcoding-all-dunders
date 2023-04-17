368e3621b284a7e3aee9bc4df24e3c15119a3837
FuexFollets <fuex9follets@gmail.com>
2023-04-17 14:49:19 -0400

    Changed container class representation and added additional math dunders

--- src/math-land/non_inplace_math.py
+++ src/math-land/non_inplace_math.py
@@ -5,78 +5,112 @@ from __future__ import annotations
 import math
 
 
-class Number:
-    value: int | float = 0
+class Land:
+    area: int | float = 0
 
-    def __init__(self, value: int | float) -> None:
-        self.value = value
+    def __init__(self, area: int | float) -> None:
+        self.area = area
 
     def __int__(self) -> int:
-        return int(self.value)
+        return int(self.area)
 
     def __float__(self) -> float:
-        return float(self.value)
+        return float(self.area)
 
-    def __add__(self, other: Number) -> Number:
-        return Number(self.value + other.value)
+    def __add__(self, other: Land) -> Land:
+        return Land(self.area + other.area)
 
-    def __sub__(self, other: Number) -> Number:
-        return Number(self.value - other.value)
+    def __sub__(self, other: Land) -> Land:
+        return Land(self.area - other.area)
 
-    def __pos__(self) -> Number:
-        return Number(+self.value)
+    def __pos__(self) -> Land:
+        return Land(+self.area)
 
-    def __neg__(self) -> Number:
-        return Number(-self.value)
+    def __neg__(self) -> Land:
+        return Land(-self.area)
 
-    def __mul__(self, other: Number) -> Number:
-        return Number(self.value * other.value)
+    def __abs__(self) -> Land:
+        return Land(abs(self.area))
 
-    def __floordiv__(self, other: Number) -> Number:
-        return Number(self.value // other.value)
+    def __mul__(self, scalar: int | float) -> Land:
+        return Land(self.area * scalar)
 
-    def __truediv__(self, other: Number) -> Number:
-        return Number(self.value / other.value)
+    def __floordiv__(self, dividend: int | float) -> Land:
+        return Land(self.area // dividend)
 
-    def __divmod__(self, other: Number) -> tuple[Number, Number]:
-        return (Number(self.value // other.value),
-                Number(self.value % other.value))
+    def __truediv__(self, dividend: int | float) -> Land:
+        return Land(self.area / dividend)
 
-    def __mod__(self, other: Number) -> Number:
-        return Number(self.value % other.value)
+    def __divmod__(self, divided_and_modded: int | float) -> tuple[Land, Land]:
+        return (Land(self.area // divided_and_modded),
+                Land(self.area % divided_and_modded))
 
-    def __floor__(self) -> Number:
-        return Number(math.floor(self.value))
+    def __mod__(self, modded: int | float) -> Land:
+        return Land(self.area % modded)
 
-    def __ceil__(self) -> Number:
-        return Number(math.ceil(self.value))
+    def __pow__(self, power: int | float) -> Land:
+        return Land(self.area ** power)
+
+    def __floor__(self) -> Land:
+        return Land(math.floor(self.area))
+
+    def __ceil__(self) -> Land:
+        return Land(math.ceil(self.area))
+
+    def __complex__(self) -> complex:
+        return complex(self.area, 0)
+
+    def __trunc__(self) -> int:
+        return int(self)
 
     def __str__(self) -> str:  # For printing
-        return str(self.value)
+        return str(self.area)
 
 
 def main():
-    val1 = Number(-2.71)
-    val2 = Number(3.14)
+    plot1 = Land(100)
+    plot2 = Land(-200)  # Owes land to the government
+    plot3 = Land(5.5)
 
     # Calls __int__ which constructs an integer
-    print(f"val1 as intenger: {int(val1)}")
+    print(f"plot1 area as intenger: {int(plot1)}")
 
     # Calls __float__ which constructs a float
-    print(f"val2 as float: {float(val1)}")
+    print(f"plot2 area as float: {float(plot2)}")
 
-    print(f"val1 + val2: {val1 + val2}")  # Calls __add__
-    print(f"val1 - val2: {val1 - val2}")  # Calls __sub__
-    print(f"+val1: {+val1}")  # Calls __pos__
-    print(f"-val1: {-val1}")  # Calls __neg__
-    print(f"val1 * val2: {val1 * val2}")  # Calls __mul__
-    print(f"val1 // val2: {val1 // val2}")  # Calls __floordiv__
-    print(f"val1 / val2: {val1 / val2}")  # Calls __truediv__
-    print(f"divmod(val1, val2): {divmod(val1, val2)}")  # Calls __divmod__
-    print(f"val1 % val2: {val1 % val2}")  # Calls __mod__
+    print(f"Combined area of plot1, plot2: {plot1 + plot3}")  # Calls __add__
 
-    print(f"math.floor(val1): {math.floor(val1)}")  # Calls __floordiv__
-    print(f"math.ceil(val1): {math.ceil(val1)}")  # Calls __ceil__
+    # Calls __sub__
+    print(f"The area of plot1 taken away from plot 2: {plot1 - plot3}")
+
+    print(f"Positive of plot1 area +(plot1.area): {+plot1}")  # Calls __pos__
+    print(f"Negative of plot1 area (-plot1): {-plot1}")  # Calls __neg__
+    print("Absolute value plot2 area: {abs(plot2)}")  # Calls __abs__
+    print(f"plot1 area being scaled by 3: {plot1 * 3}")  # Calls __mul__
+
+    # Calls __floordiv__
+    print(f"Divisions into plots of land from plot1 of exactly 9 area:"
+          f"{plot1 // 9}")
+    print(f"Divides plot2 into 5 pieces: {plot2 / 5}")  # Calls __truediv__
+
+    divisions, remainder = divmod(plot1, 7)  # Calls __divmod__
+    print(f"plot1 can be divided into {divisions} plots of 7 area each"
+          f"with a remainder of {remainder} area")
+
+    # Calls __mod__
+    print(f"The remainder of dividing plot1 into 22 pieces is {plot1 % 22}")
+
+    # Calls __floor__
+    print(f"The area of plot3 rounded down is: {math.floor(plot3)}")
+
+    # calls __ceil__
+    print(f"The are of plot3 rounded up is: {math.ceil(plot3)}")
+
+    # calls __complex__
+    print(f"Area of plot3 represented as a complex number {complex(plot3)}")
+
+    # calls __trunc__
+    print(f"The truncated integer area of plot3 {math.trunc(plot3)}")
 
 
 if __name__ == "__main__":
