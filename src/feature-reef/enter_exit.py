878c701a0a9dbceb7c95c7bdbf362d4e890c123a
Rajin Braynard <85237518+FuexFollets@users.noreply.github.com>
2023-04-16 20:42:24 -0400

    Context manager dunders (#2)
    Co-authored-by: trag1c <trag1cdev@yahoo.com>

---
+++ src/feature-reef/enter_exit.py
@@ -0,0 +1,38 @@
+# Dunders __enter__ and __exit__
+
+from __future__ import annotations
+
+
+class Context:
+    def fancy_function(self) -> None:
+        print("executing fancy function")
+
+    def __enter__(self) -> Context:
+        print("entering with block")
+
+        return self
+
+    def __exit__(
+            self, exception_type, exception_value,
+            exception_traceback) -> bool:
+        print("exiting with block")
+
+        if (exception_value is not None):
+            print(f"exception '{exception_type}' "
+                  f"occurred at line {exception_traceback.tb_lineno}: "
+                  f"{exception_value}")
+
+            print("ignoring error")
+
+        return True  # Returning true should suppress the exception
+
+
+def main() -> None:
+    with Context() as ctx:  # Initializes ctx as a Context object
+        ctx.fancy_function()
+        raise Exception("error")
+    # Context.__exit__ suppresses the exception
+
+
+if __name__ == "__main__":
+    main()
