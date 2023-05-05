d9f7b1e8d44ed35f9f7e4d671c23559cf6235b8b
FuexFollets <fuex9follets@gmail.com>
2023-05-05 09:17:57 -0400

    Changed land exmple to number wrapper, fixed strings and variable names

--- src/math-land/non_inplace_math.py
+++ src/math-land/non_inplace_math.py
@@ -7,107 +7,107 @@ from __future__ import annotations
 import math
 
 
-class Land:
-    area: int | float = 0
+class NumberWrapper:
+    value: int | float = 0
 
-    def __init__(self, area: int | float) -> None:
-        self.area = area
+    def __init__(self, value: int | float) -> None:
+        self.value = value
 
-    def __add__(self, other: Land) -> Land:
-        return Land(self.area + other.area)
+    def __add__(self, other: NumberWrapper) -> NumberWrapper:
+        return NumberWrapper(self.value + other.value)
 
-    def __sub__(self, other: Land) -> Land:
-        return Land(self.area - other.area)
+    def __sub__(self, other: NumberWrapper) -> NumberWrapper:
+        return NumberWrapper(self.value - other.value)
 
-    def __pos__(self) -> Land:
-        return Land(+self.area)
+    def __pos__(self) -> NumberWrapper:
+        return NumberWrapper(+self.value)
 
-    def __neg__(self) -> Land:
-        return Land(-self.area)
+    def __neg__(self) -> NumberWrapper:
+        return NumberWrapper(-self.value)
 
-    def __abs__(self) -> Land:
-        return Land(abs(self.area))
+    def __abs__(self) -> NumberWrapper:
+        return NumberWrapper(abs(self.value))
 
-    def __mul__(self, scalar: int | float) -> Land:
-        return Land(self.area * scalar)
+    def __mul__(self, scalar: int | float) -> NumberWrapper:
+        return NumberWrapper(self.value * scalar)
 
-    def __floordiv__(self, dividend: int | float) -> Land:
-        return Land(self.area // dividend)
+    def __floordiv__(self, divisor: int | float) -> NumberWrapper:
+        return NumberWrapper(self.value // divisor)
 
-    def __truediv__(self, dividend: int | float) -> Land:
-        return Land(self.area / dividend)
+    def __truediv__(self, divisor: int | float) -> NumberWrapper:
+        return NumberWrapper(self.value / divisor)
 
-    def __divmod__(self, divided_and_modded: int | float) -> tuple[Land, Land]:
+    def __divmod__(self, divisor: int | float) -> tuple[NumberWrapper, NumberWrapper]:
         return (
-            Land(self.area // divided_and_modded),
-            Land(self.area % divided_and_modded),
+            NumberWrapper(self.value // divisor),
+            NumberWrapper(self.value % divisor),
         )
 
-    def __mod__(self, modded: int | float) -> Land:
-        return Land(self.area % modded)
+    def __mod__(self, divisor: int | float) -> NumberWrapper:
+        return NumberWrapper(self.value % divisor)
 
-    def __pow__(self, power: int | float) -> Land:
-        return Land(self.area**power)
+    def __pow__(self, power: int | float) -> NumberWrapper:
+        return NumberWrapper(self.value**power)
 
-    def __floor__(self) -> Land:
-        return Land(math.floor(self.area))
+    def __floor__(self) -> NumberWrapper:
+        return NumberWrapper(math.floor(self.value))
 
-    def __ceil__(self) -> Land:
-        return Land(math.ceil(self.area))
+    def __ceil__(self) -> NumberWrapper:
+        return NumberWrapper(math.ceil(self.value))
 
     def __trunc__(self) -> int:
-        return int(self.area)
+        return int(self.value)
 
     def __str__(self) -> str:  # For printing
-        return str(self.area)
+        return str(self.value)
 
 
 def main():
-    plot1 = Land(100)
-    plot2 = Land(-200)  # Owes land to the government
-    plot3 = Land(5.5)
+    number_100 = NumberWrapper(100)
+    number_neg200 = NumberWrapper(-200)  # Owes land to the government
+    number_5point5 = NumberWrapper(5.5)
 
-    print(f"Combined area of plot1, plot2: {plot1 + plot3}")  # Calls __add__
+    print(f"Combined value of number_100, number_neg200: {number_100 + number_5point5}")  # Calls __add__
 
     # Calls __sub__
-    print(f"The area of plot1 taken away from plot 2: {plot1 - plot3}")
+    print(f"The value of number_100 taken away from plot 2: {number_100 - number_5point5}")
 
-    print(f"Positive of plot1 area +(plot1.area): {+plot1}")  # Calls __pos__
-    print(f"Negative of plot1 area -(plot1): {-plot1}")  # Calls __neg__
-    print("Absolute value plot2 area: {abs(plot2)}")  # Calls __abs__
-    print(f"plot1 area being scaled by 3: {plot1 * 3}")  # Calls __mul__
+    print(f"Positive of number_100 value +(number_100.value): {+number_100}")  # Calls __pos__
+    print(f"Negative of number_100 value -(number_100): {-number_100}")  # Calls __neg__
+    print(f"Absolute value number_neg200 value: {abs(number_neg200)}")  # Calls __abs__
+    print(f"number_100 value being scaled by 3: {number_100 * 3}")  # Calls __mul__
 
     # Calls __pow__
-    print(f"plot3 area being raised to the power of 3 {plot3 ** 3}")
-    # pow(plot3, 3) also works
+    print(f"number_5point5 value being raised to the power of 3 {number_5point5 ** 3}")
+    # pow(number_5point5, 3) also works
 
     # Calls __floordiv__
     print(
-        f"Divisions into plots of land from plot1 of exactly 9 area: "
-        f"{plot1 // 9}"
+        "Divisions into plots of land from number_100 of exactly 9 value: "
+        f"{number_100 // 9}"
     )
-    print(f"Divides plot2 into 5 pieces: {plot2 / 5}")  # Calls __truediv__
+    print(f"Divides number_neg200 into 5 pieces: {number_neg200 / 5}")  # Calls __truediv__
 
-    divisions, remainder = divmod(plot1, 7)  # Calls __divmod__
+    divisions, remainder = divmod(number_100, 7)  # Calls __divmod__
     print(
-        f"plot1 can be divided into {divisions} plots of 7 area each "
-        f"with a remainder of {remainder} area"
+        f"number_100 can be divided into {divisions} plots of 7 value each "
+        f"with a remainder of {remainder} value"
     )
 
     # Calls __mod__
     print(
-        f"The remainder of dividing plot1 into 22 whole pieces is"
-        f"{plot1 % 22}"
+        f"The remainder of dividing number_100 into 22 whole pieces is"
+        f"{number_100 % 22}"
     )
 
     # Calls __floor__
-    print(f"The area of plot3 rounded down is: {math.floor(plot3)}")
+    print(f"The value of number_5point5 rounded down is: {math.floor(number_5point5)}")
 
     # calls __ceil__
-    print(f"The are of plot3 rounded up is: {math.ceil(plot3)}")
+    print(f"The are of number_5point5 rounded up is: {math.ceil(number_5point5)}")
 
     # calls __trunc__
-    print(f"The truncated integer area of plot3 {math.trunc(plot3)}")
+    print(f"The truncated integer value of number_5point5 {math.trunc(number_5point5)}")
 
 
 if __name__ == "__main__":
