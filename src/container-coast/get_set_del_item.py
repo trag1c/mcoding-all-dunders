c7de4ef7934962373447976d313a03700ef41094
FuexFollets <fuex9follets@gmail.com>
2023-05-06 09:58:30 -0400

    Implemented binary tree, added examples for dunders and fallback implementations

--- src/container-coast/get_set_del_item.py
+++ src/container-coast/get_set_del_item.py
@@ -1,49 +1,138 @@
 # Dunders __getitem__, __setitem__, __delitem__
+# __missing__, __iter__, __next__, __reversed__, __len__, __contains__
 
 from __future__ import annotations
 
+from random import randint
 
-class LunchBoxes:
-    def __init__(self, **names_to_items: list[str]) -> None:
-        self.user_items = names_to_items
 
-    def __getitem__(self, name_or_index: str | int) -> str | tuple[str, str]:
-        """Returns the value of a key or a pair at index when the dict is sorted"""
+class BinaryTree:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
 
-        if isinstance(name_or_index, int):
-            return sorted(self.user_items.items())[name_or_index]
+    def __missing__(self, key):
+        raise KeyError(f"Key {key} not found")
 
-        return self.user_items[name_or_index]
+    def __contains__(self, key):
+        if self.key == key:
+            return True
 
-    def __setitem__(self, name: str, new_items: str) -> None:
-        self.user_items[name] = new_items
+        if self.left is not None and key < self.key:
+            return key in self.left  # Calls __contains__
 
-    def __delitem__(self, name: str) -> None:
-        del self.user_items[name]
+        if self.right is not None and self.key < key:
+            return key in self.right  # Calls __contains__
+
+        return False
+
+    def __setitem__(self, key, value):
+        if (self.key is None) or (self.key == key):
+            self.key = key
+            self.value = value
+
+        if (key < self.key):
+            self.left = BinaryTree() if self.left is None else self.left
+            self.left[key] = value
+
+        if (self.key < key):
+            self.right = BinaryTree() if self.right is None else self.right
+            self.right[key] = value
+
+    def __getitem__(self, key):
+        if self.key == key:
+            return self.value
+
+        if key not in self:
+            return self.__missing__(key)
+
+        if key < self.key:
+            return self.left[key]
+
+        return self.right[key]
+
+    def __delitem__(self, key):
+        if key in self:
+            self[key] = None
+        else:
+            self.__missing__(key)
+
+    def __len__(self):
+        if self.key is None:
+            return 0
+
+        return 1 + (len(self.left) if self.left is not None else 0) + (len(self.right) if self.right is not None else 0)
+
+    def __iter__(self):
+        if self.left is not None:
+            yield from self.left
+
+        yield (self.key, self.value)
+
+        if self.right is not None:
+            yield from self.right
+
+    def __reversed__(self):
+        if self.right is not None:
+            yield from reversed(self.right)
+
+        yield (self.key, self.value)
+
+        if self.left is not None:
+            yield from reversed(self.left)
+
+
+class ListWrapper:
+    def __init__(self, value=[]):
+        self.value = value
+
+    def __getitem__(self, index):
+        return self.value[index]
+
+    def __len__(self):
+        return len(self.value)
 
 
 def main():
-    lunchboxes = LunchBoxes(
-        alice=["apple", "carrot", "rice"],
-        bob=["asparagus", "lemon", "fish"],
-        carol=["chocolate", "milk", "bread"],
-    )
+    tree = BinaryTree()
 
-    print(f"Carol's lunch box: {lunchboxes['carol']}")  # A call to __getitem__
-    print(f"Bob's lunch box: {lunchboxes['bob']}")  # A call to __getitem__
+    for _ in range(20):
+        tree[randint(0, 10)] = randint(0, 50)  # Calls __setitem__
 
-    lunchboxes["alice"] = [
-        "apple",
-        "carrot",
-        "rice",
-        "chocolate",
-    ]  # A call to __setitem__
+    print("Tree in order by keys:")
+    for key, value in tree:  # Calls __iter__
+        print(f"{key}: {value}")
 
-    print(f"Alice's new lunchbox {lunchboxes['alice']}")
+    print("Tree in reverse order:")  # Calls __reversed__
+    for key, value in reversed(tree):
+        print(f"{key}: {value}")
 
-    del lunchboxes["carol"]  # A call to __delitem__
+    print(f"Tree has {len(tree)} nodes")  # Calls __len__
+    print(f"Tree has 8 as a key: {8 in tree}")  # Calls __contains__
 
-    print(f"Current lunchboxes data: {lunchboxes.user_items}")
+    try:
+        # Calls __getitem__ but fails and calld
+        # __missing__ which raises KeyError
+        print(tree[100])
+
+    except KeyError as e:
+        print(f"Error when trying to access key 100: {e}")
+
+    tree[24] = 150  # Calls __setitem__
+    print(f"The tree at key 24 is {tree[24]}")  # Calls __getitem__
+
+    # __iter__ and __reversed__ fallback
+
+    lw = ListWrapper([1, 2, 3, 4, 5])
+
+    # Still works even without __iter__, uses __getitem__ and __len__
+    for number in lw:
+        print(number, end=" ")
+
+    for number in reversed(lw):
+        print(number, end=" ")
 
 
 if __name__ == "__main__":
