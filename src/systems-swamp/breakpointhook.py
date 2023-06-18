e3e794bfb6e23ddacd26aebbbb047ec0dd98796f
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2023-06-18 20:50:36 +1200

    Update breakpointhook.py
    girlies – I am struggling with this one

--- src/systems-swamp/breakpointhook.py
+++ src/systems-swamp/breakpointhook.py
@@ -1,19 +1,72 @@
+"""Adds locals to breaks?"""
 import contextlib
+import linecache
+import reprlib
 import sys
+from pathlib import Path
+from pdb import Pdb
+from pprint import pformat
+from textwrap import indent
+
+
+class PdbLocals(Pdb):
+    @staticmethod
+    def _short_path(filename) -> Path:
+        """
+        Returns a relative path to the frame file if possible or an absolute path
+        """
+        try:
+            return Path(filename).resolve().relative_to(Path.cwd())
+        except ValueError:
+            return Path(filename).resolve()
+
+    def format_stack_entry(self, frame_lineno, lprefix=': '):
+        frame, lineno = frame_lineno
+        name = frame.f_code.co_name if frame.f_code.co_name else "<lambda>"
+        filename = PdbLocals._short_path(frame.f_code.co_filename)
+        lines = [f"File \"{filename}\" line {lineno} in {name}", ""]
+
+        if lineno is not None:
+            path = self.canonic(frame.f_code.co_filename)
+            if line := linecache.getline(path, lineno, frame.f_globals):
+                lines += [indent(line.strip(), " " * 4), ""]
+
+        lines += [f"Locals: {pformat(self.curframe_locals)}"]
+
+        if '__return__' in frame.f_locals:
+            rv = frame.f_locals['__return__']
+            lines += ["Returning -> " + reprlib.repr(rv)]
+        lines += [""]
+
+        return "\n".join(lines)
+
+
+def set_trace(*, header=None):
+    pdb = PdbLocals()
+    if header is not None:
+        pdb.message(header)
+    pdb.set_trace(sys._getframe().f_back)
 
 
 @contextlib.contextmanager
-def no_breakpoint():
-    sys.breakpointhook = lambda *args, **kwargs: print(
-        "You didn't think I'd let you debug so easily, did you?",
-        file=sys.stderr,
-    )
+def locals_debugger():
+    sys.breakpointhook = set_trace
     yield
     sys.breakpointhook = sys.__breakpointhook__
 
 
-with no_breakpoint():
-    breakpoint()
-    x = 1
-    y = x * 2
-    print(y)
+# Example
+def foo(x, y):
+    z = x + y
+    return z
+
+
+def main():
+    with locals_debugger():
+        breakpoint()
+        result = foo(1, 1)
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
