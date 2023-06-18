a9c07dfc4482f5345d443d7383a5ee1d6e61b6a7
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2023-06-19 11:07:13 +1200

    Update breakpointhook.py

--- src/systems-swamp/breakpointhook.py
+++ src/systems-swamp/breakpointhook.py
@@ -1,4 +1,5 @@
-"""Adds locals to breaks?"""
+"""Adds locals to default Pdb debugger at `breakpoint()`"""
+import contextlib
 import linecache
 import reprlib
 import sys
@@ -8,35 +9,45 @@ from pprint import pformat
 from textwrap import indent
 
 
-def _short_path(filename) -> Path:
-    try:
-        return Path(filename).resolve().relative_to(Path.cwd())
-    except ValueError:
-        return Path(filename).resolve()
-
-
 class PdbLocals(Pdb):
     """Pdb... with locals (and some minor formatting)"""
 
-    def format_stack_entry(self, frame_lineno, lprefix=": "):
-        frame, lineno = frame_lineno
+    def _short_path(self, filename) -> Path:
+        try:
+            return Path(filename).resolve().relative_to(Path.cwd())
+        except ValueError:
+            return Path(filename).resolve()
 
+    def _header(self, frame, lineno):
         name = frame.f_code.co_name if frame.f_code.co_name else "<lambda>"
-        filename = _short_path(frame.f_code.co_filename)
-        lines = [f'File "{filename}" line {lineno} in {name}', ""]
+        filename = self._short_path(frame.f_code.co_filename)
+        return f'File "{filename}" line {lineno} in {name}'
 
+    def _code_line(self, frame, lineno):
         if lineno is not None:
             path = self.canonic(frame.f_code.co_filename)
             if line := linecache.getline(path, lineno, frame.f_globals):
-                lines += [indent(line.strip(), " " * 4), ""]
+                return indent(line.strip(), " " * 4)
 
-        lines += [f"Locals: {pformat(self.curframe_locals)}"]
-
+    def _return_value(self, frame):
         if "__return__" in frame.f_locals:
             rv = frame.f_locals["__return__"]
-            lines += ["Returning -> " + reprlib.repr(rv)]
-        lines += [""]
+            return "Returning -> " + reprlib.repr(rv)
 
+    def _locals(self):
+        return {
+            k: v for k, v in self.curframe_locals.items() if k != "__return__"
+        }
+
+    def format_stack_entry(self, frame_lineno, lprefix=": "):
+        frame, lineno = frame_lineno
+        lines = [self._header(frame, lineno) + "\n"]
+        if line := self._code_line(frame, lineno):
+            lines.append(line + "\n")
+        lines.append(f"Locals: {pformat(self._locals())}")
+        if return_value := self._return_value(frame):
+            lines.append(return_value)
+        lines.append("")
         return "\n".join(lines)
 
 
@@ -47,11 +58,10 @@ def set_trace(*, header=None):
     pdb.set_trace(sys._getframe().f_back)
 
 
+@contextlib.contextmanager
 def debug_with_locals():
     sys.breakpointhook = set_trace
-
-
-def reset_debug():
+    yield
     sys.breakpointhook = sys.__breakpointhook__
 
 
@@ -68,5 +78,7 @@ def main():
 
 
 if __name__ == "__main__":
-    debug_with_locals()
+    # enter "continue" before main finishes the first time
+    with debug_with_locals():
+        main()
     main()
