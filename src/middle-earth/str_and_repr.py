dbbb57c57726c9348eb790f45da89d7a4444c82a
Yoshikage Kira <trevorphillips@gmx.us>
2023-06-23 12:42:54 -0400

    Added the ascii function and its f-string example in the code

--- src/middle-earth/str_and_repr.py
+++ src/middle-earth/str_and_repr.py
@@ -1,5 +1,5 @@
 class MyCoolClass:
-    def __init__(self, *, value: int) -> None:
+    def __init__(self, *, value: str) -> None:
         self.value = value
 
     def __repr__(self) -> str:
@@ -12,14 +12,18 @@ class MyCoolClass:
 
 
 if __name__ == "__main__":
-    my_class = MyCoolClass(value=10)
+    my_class = MyCoolClass(value="© OpenAI ®")
 
     print("Printing the output from __repr__()")
     class_repr = repr(my_class)
     print("The repr is:", class_repr)
     # Evaling the repr should give us an identical object.
     print("The evaled repr is:", repr(eval(class_repr)))
-    print(f"Calling the repr using f-string: {my_class!r}\n")
+    print(f"Calling the repr using f-string: {my_class!r}")
+
+    # You can use ascii to escape the non-printable characters in the repr
+    print("The ascii repr of my_class is:", ascii(my_class))
+    print(f"Calling the ascii using f-string: {my_class!a}\n")
 
     # Following are the ways to call the __str__
     print("Printing the output from __str__()")
