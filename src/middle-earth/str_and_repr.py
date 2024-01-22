013e3781dfcfc509894e2fa310e72ff7def34e4f
Yoshikage Kira <65515165+isFakeAccount@users.noreply.github.com>
2024-01-22 13:00:22 -0500

    Added the dunder str to repr.py and updated the file name (#27)
    * Added the dunder str to repr.py and updated the file name
    
    - Added the __str_() in the repr.py
    - Updated the repr.py filename to str_and_repr.py
    
    * Added the ascii function and its f-string example in the code
    
    * Added comment requested by trag1c
    
    * Made black not cry
    
    * Corrected Doc-String for repr
    
    Corrected Doc-String for repr based from PR feedback
    
    * Elaborated on eval and repr
    
    Elaborated on when evaling a repr should give us identical object and also the cases when it doesn't
    
    * Fixed formatting issues
    
    Made black not cry
    
    * Edit __repr__ doc str
    
    - Edited __repr__ doc 
    - Added EOL in gitignore
    
    * 2nd attempt at adding EOL
    
    * Code in comment is now uncommented
    
    - Removed extra line from .gitignore
    - Converted lambda to function since ruff was complaining
    
    * Removed docs and comments
    
    Removed docs, comments, and some code, so the file can fit on screen.
    
    * Removed args only limitation from init
    
    Removed args only limitation from init

---
+++ src/middle-earth/str_and_repr.py
@@ -0,0 +1,32 @@
+import re
+
+
+class MyCoolClass:
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"MyCoolClass(value={self.value!r})"
+
+    def __str__(self) -> str:
+        return f"My Cool Class Value is: {self.value}"
+
+
+if __name__ == "__main__":
+    my_class = MyCoolClass(value="∫ f(x) dx")
+    print("Printing the output from __repr__()")
+    print("The repr is:", repr(my_class))
+    print(f"Calling the repr using f-string: {my_class!r}")
+    print("The evaled repr is:", repr(eval(repr(my_class))))
+
+    print(repr(re.compile(r"\d")))  # Can't eval repr in these examples
+    o = object()
+    print(repr(o))
+
+    print("The ascii repr of my_class is:", ascii(my_class))
+    print(f"Calling the ascii using f-string: {my_class!a}\n")
+
+    print("Printing the output from __str__()")
+    print(my_class)
+    print("The str() output is:", str(my_class))
+    print(f"Calling the str using f-string: {my_class!s} == {my_class}")
